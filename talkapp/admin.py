2625200c198f0b3f59cb1a4c25fbcc56ad5905bd
おーしろ <sabitetsu.a24h@gmail.com>
2021-11-15 01:06:26 +0900

    model作成

--- talkapp/admin.py
+++ talkapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from talkapp.models import TalkModel
+
 # Register your models here.
+
+@admin.register(TalkModel)
+class TalkModel(admin.ModelAdmin):
+    pass
--- talkapp/models.py
+++ talkapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TalkModel(models.Model):
+    inputWord = models.CharField(max_length=100)
+    outputWord = models.CharField(max_length=200)
