fa3697e39bbbf45f92a04e400c89290134cfc529
おーしろ <sabitetsu.a24h@gmail.com>
2021-11-15 03:42:49 +0900

    C:/Program Files/Git/talk/talkmodel でjson取得

--- talkapp/urls.py
+++ talkapp/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from rest_framework import routers
+
+from talkapp.views import TalkModelViewSet
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+router = routers.DefaultRouter()
+router.register(r'talkmodel', TalkModelViewSet)
+urlpatterns = router.urls
--- talkapp/views.py
+++ talkapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import TalkModel
+from .serializer import TalkModelSerializer
+
+class TalkModelViewSet(viewsets.ModelViewSet):
+    queryset = TalkModel.objects.all()
+    serializer_class = TalkModelSerializer
+
