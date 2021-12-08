5ac51de221380b22edbd348d51c29e15bd589471
おーしろ <sabitetsu.a24h@gmail.com>
2021-12-09 00:43:56 +0900

    [fix] indexの追加とデプロイfix

--- kizunachannel/urls.py
+++ kizunachannel/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+from kizunachannel.views import index
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('talk/', include('talkapp.urls')),
     path('web/', include('webpage.urls')),
