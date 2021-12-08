19c8a61b0fc5e39d8f58e9b0f279dc341fac19a8
おーしろ <sabitetsu.a24h@gmail.com>
2021-12-09 02:18:40 +0900

    fav調査

--- kizunachannel/urls.py
+++ kizunachannel/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from kizunachannel.views import index
+from kizunachannel.views import index, fav
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('favicon.ico/', views.fav, name='fav'),
     path('admin/', admin.site.urls),
     path('talk/', include('talkapp.urls')),
     path('web/', include('webpage.urls')),
--- kizunachannel/views.py
+++ kizunachannel/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def fav(request):
+    return render(request, 'fav.html')
