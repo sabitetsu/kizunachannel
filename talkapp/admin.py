bd706f20d47ccf2d636625d99c50c9b98a343dbf
おーしろ <sabitetsu.a24h@gmail.com>
2021-12-11 07:02:29 +0900

    まばたき追加

--- talkapp/admin.py
+++ talkapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from talkapp.models import TalkModel
+from talkapp.models import TalkModel, UserModel, UserLike
 
 # Register your models here.
 
 @admin.register(TalkModel)
 class TalkModel(admin.ModelAdmin):
     pass
+
+admin.site.register(UserModel)
+admin.site.register(UserLike)
--- talkapp/models.py
+++ talkapp/models.py
@@ -6,3 +6,13 @@ class TalkModel(models.Model):
     
     def __str__(self):
         return self.inputWord
+
+class UserModel(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    old = models.IntegerField()
+    friendry = models.IntegerField()
+
+class UserLike(models.Model):
+    food = models.CharField(max_length=100)
+    music = models.CharField(max_length=100)
